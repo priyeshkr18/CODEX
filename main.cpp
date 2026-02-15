bbbcf4c96ff02389600faf3be9f819e254e30f81
Priyesh Choudhary <143314183+priyeshkr18@users.noreply.github.com>
2026-02-15 12:02:59 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num1;
+    int num2;
+
+    cout<<"Enter num1: ";
+    cin>>num1;
+    cout<<"Enter num2: ";
+    cin>>num2;
+
+    int sum = (num1 + num2);
+    int prod = (num1 * num2);
+    cout<<"sum = "<<sum<<endl;
+    cout<<"prod = "<<prod<<endl;
+
+    return 0;
+}
