736c792c995b559bd89d1dda179d58b8d2c1ffab
Priyesh Choudhary <143314183+priyeshkr18@users.noreply.github.com>
2026-02-21 13:31:58 +0530

    Add files via upload

---
+++ primeno.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n = 9;
+	bool isPrime = true;
+
+	for (int i = 2; i < n - 1; i++)
+	{
+		if (n % i == 0)
+		{
+			isPrime = false;
+			break;
+		}
+	}
+
+	if (isPrime == true)
+	{
+		cout << "Prime" << endl;
+	}
+	else
+	{
+		cout << "Not Prime" << endl;
+	}
+	return 0;
+}
